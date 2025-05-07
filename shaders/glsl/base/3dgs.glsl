521fdf46c0788a384c9a808fef75be23848205a4
KJS <goliath917@gmail.com>
2025-05-07 20:37:02 +0900

    [JS] ADD : 3DGRT raygen shader (need compile test)

---
+++ shaders/glsl/base/3dgs.glsl
@@ -0,0 +1,38 @@
+/*
+ * Abura Soba, 2025
+ *
+ * define.glsl
+ *
+ */
+
+#define EPS_T 1e-9
+#define MAX_HIT_PER_TRACE 16
+#define INVALID_PARTICLE_ID 0xFFFFFFFF
+#define INFINITE_DISTANCE 1e20f
+
+struct Aabb {
+	float minX, minY, minZ;
+	float maxX, maxY, maxZ;
+};
+
+struct ParticleDensity {
+	vec3 position;
+	float density;
+	vec4 quaternion;
+	vec3 scale;
+	float padding;
+};
+
+struct Param {
+	Aabb aabb;
+
+	float minTransmittance;
+
+	ParticleDensity particleDensity;
+	float particleRadiance;
+	float hitMinGaussianResponse;
+	float alphaMinThreshold;
+	unsigned int sphDegree;
+
+	float particleVisibility;
+}
